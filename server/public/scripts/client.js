e619603414d76dbee2d1b7519c54948b18cc58ef
Kristine Jensen <kristine.b.jensen@gmail.com>
2017-03-04 14:07:54 -0600

    Working on complete and delete buttons

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -7,7 +7,19 @@ $(document).ready(function(){
     $('#displayNewTask').append($('#newTaskName').val());
     $('#displayNewTaskDetails').append($('#newTaskDetails').val());
     $('#displayNewTaskDueDate').append($('#newTaskDueDate').val());
+    $('#displayCompleteButton').append('<button type="button" id="completeButton">Complete</button>');
+    $('#displayDeleteButton').append('<button type="button" id="deleteButton">Delete</button>');
+
+    $('#completeButton').on('click', function(){
+    }); // end of complete button listener
+
+    $('#deleteButton').on('click', function(){
+    }); // end of delete button listener
+
   }); // end of create task button listener
+
+
+
 
 
 
