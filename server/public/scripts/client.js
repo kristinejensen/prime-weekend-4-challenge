81bc85dc7066fae63a40f0b1bf0d56e830926550
Kristine Jensen <kristine.b.jensen@gmail.com>
2017-03-04 15:05:24 -0600

    Working on sending task info to database

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,25 +2,44 @@ console.log('client.js sourced');
 $(document).ready(function(){
   console.log('jquery is sourced');
 
-  $('#createTaskButton').on('click', function(){
-    console.log('button clicked');
-    $('#displayNewTask').append($('#newTaskName').val());
-    $('#displayNewTaskDetails').append($('#newTaskDetails').val());
-    $('#displayNewTaskDueDate').append($('#newTaskDueDate').val());
-    $('#displayCompleteButton').append('<button type="button" id="completeButton">Complete</button>');
-    $('#displayDeleteButton').append('<button type="button" id="deleteButton">Delete</button>');
-
-    $('#completeButton').on('click', function(){
-    }); // end of complete button listener
-
-    $('#deleteButton').on('click', function(){
-    }); // end of delete button listener
-
-  }); // end of create task button listener
-
-
+$('#createTaskButton').on('click', function(){
+  var newTaskObject = {};
+  newTaskObject.taskName = $('#newTaskName').val();
+  newTaskObject.taskDetails = $('#newTaskDetails').val();
+  newTaskObject.dueDate = $('#newTaskDueDate').val();
+  newTaskObject.complete = null;
+  console.log(newTaskObject);
+}); // end of click listener
 
 
 
 
 }); // end of document ready
+
+// dispalyAllTasks(); // calling dispalyAllTasks function when page loads to display full task list
+// function displayAllTasks(){
+//   $.ajax({
+//     type: 'GET',
+//     url: '/tasks',
+//     success: function(response) {
+//       $('#taskListDisplay').empty();
+//   $('#createTaskButton').on('click', function(){
+//     console.log('button clicked');
+//     $('#displayNewTask').append($('#newTaskName').val());
+//     $('#displayNewTaskDetails').append($('#newTaskDetails').val());
+//     $('#displayNewTaskDueDate').append($('#newTaskDueDate').val());
+//     $('#displayCompleteButton').append('<button type="button" id="completeButton">Complete</button>');
+//     $('#displayDeleteButton').append('<button type="button" id="deleteButton">Delete</button>');
+//   }); // end of create task button listener
+// } // end of success function
+// }); // end of ajax call
+// } // end of dispalyAllTasks function
+
+
+// $('#completeButton').on('click', function(){
+//   console.log('complete button clicked');
+// }); // end of complete button listener
+//
+// $('#deleteButton').on('click', function(){
+//   console.log('delete button clicked');
+// }); // end of delete button listener
---
+++ weekend-4-markdown.md
@@ -0,0 +1,12 @@
+---CLIENT SIDE LOGIC---
+
+- [x] create front-end experience that allows a user to create a task
+
+---SERVER SIDE LOGIC---
+
+- [ ] this is an incomplete item
+
+
+---DATABASE---
+
+- [ ] when the task is created, it should be stored inside of a database
